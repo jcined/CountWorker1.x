a79e0f9ffcfb1ef21426734f1b31228f346e16e5
Jcined <102638772+jcined@users.noreply.github.com>
2023-01-16 15:07:38 +0800

    Add files via upload

--- backend/func.py
+++ backend/func.py
@@ -3,8 +3,8 @@ import json
 import os
 import random
 import base64
-import socket
 import sqlite3
+import requests
 
 
 # 响应
@@ -88,9 +88,7 @@ class UpDate:
 
 # 获取IP地址
 def get_local_ip() -> str:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('8.8.8.8', 80))
-    return s.getsockname()[0]
+    return requests.get('https://checkip.amazonaws.com').text.strip()
 
 
 # 封装ws数据
