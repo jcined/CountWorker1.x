627e1b6204a144cd8c9813bcf23146d86c56dca6
Jcined <102638772+jcined@users.noreply.github.com>
2023-02-01 09:08:46 +0800

    update

---
+++ backend/strategy/88600/chart.py
@@ -0,0 +1,29 @@
+state = [
+    [
+        {
+            "title": "账户信息",
+            "tables": {
+                "cols": ["初始余额", "当前余额", "总收益", "持仓数量", "当前价格", "目标买价", "目标卖价"],
+                "rows": [],
+            }
+        },
+    ],
+    [
+        {
+            "button": {
+                "btn": "清空收益",
+                "style": "danger",
+                "key": "clearProfit",
+            }
+        }
+    ],
+    [
+        {
+            "button": {
+                "btn": "清空日志",
+                "style": "danger",
+                "key": "clearLog",
+            }
+        }
+    ],
+]
